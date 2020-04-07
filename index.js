393ac136f8fbcd31c3dd4ef5a74cafb0236b4bae
jenniferanoneill <63299697+jenniferanoneill@users.noreply.github.com>
2020-04-07 14:38:26 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let name;
+let age;
+let jobRole = "Student"
+
+function personalData(name, age) {
+    console.log("Hello, I am " + name + " and I am " + age + " years old.")
+}
+
+personalData("Jennifer", 15)
+
+let numberOne;
+let numberTwo;
+
+function addition(numberOne, numberTwo) {
+    let total= numberOne + numberTwo
+    console.log(total)
+}
+
+addition(50, 40)
+addition(30, 50)
+
